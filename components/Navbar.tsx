bd0680273f80e3f49b18ca4703fa43644de96aa0
Khemiri-sahar <sahar.khemiri@insat.ucar.tn>
2025-11-25 20:02:35 +0100

    feat: add auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,29 @@
-import { Link } from "lucide-react"
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs" 
+import Link from "next/link"
 import Image from "next/image"
 import NavItems from "./NavItems"   
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-        <a href="/"> 
+        <Link href="/"> 
             <div className="flex items-center gap-2.5 cursor-pointer" >
                 <Image 
                     src="/images/logo.svg" 
                     alt="logo" width={46} height={44} 
                 />
             </div>
-        </a>
+        </Link>
         <div className="flex items-center gap-8">
             <NavItems />
-            <p>Sign In</p>
+            <SignedOut> 
+                <SignInButton>
+                    <button className="btn-signin">Sign In</button>
+                </SignInButton>
+            </SignedOut>
+            <SignedIn>
+                <UserButton afterSignOutUrl="/" />
+            </SignedIn>
         </div>
     </nav>
   )
